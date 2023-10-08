7f30a1076544fa0ca9a87cb9e94af92b2bdb50e3
Katarzyna Barska <104527962+Kasia7891@users.noreply.github.com>
2023-10-08 10:23:34 +0200

    Added React Router and file: SeedsTable and SeedTableRow

---
+++ src/components/SeedsTable.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {SeedEntity} from '../../../../Backend/types';
+import {SeedTableRow} from "./SeedTableRow";
+
+interface Props {
+    seeds: SeedEntity[];
+    onSeedsChange?: () => void;
+}
+
+export const SeedsTable = (props: Props) => (
+
+    <table>
+        <thead>
+        <tr>
+            <th>
+                Nazwa dodatku
+            </th>
+            <th>
+                Cena dodatku
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+        {
+            props.seeds.map(seed => (
+
+                <SeedTableRow
+                seed={seed}
+                key={seed.idSeed}
+                onSeedsChange={props.onSeedsChange}
+                />
+
+            ))
+
+        }
+        </tbody>
+    </table>
+
+);
