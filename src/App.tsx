846c442b3a01714dcf6af8e5df5b1a4929281149
Katarzyna Barska <104527962+Kasia7891@users.noreply.github.com>
2023-10-10 15:02:20 +0200

    Added Routes to App.tsx: '/', '/seed','/order'.

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,20 @@
-import React, {useEffect, useState} from 'react';
-import { SeedEntity } from 'types';
-import './App.css';
-import {Header} from "./components/Header/Header";
-import {SeedsTable} from "./components/SeedsTable";
-import {SeedsListTest} from "./components/seedsListTest";
-import {SeedTableRow} from "./components/SeedTableRow";
+import React from 'react';
+import {SeedsList} from "./components/Seeds/SeedsList";
 import {Route, Routes} from "react-router-dom";
+import {HomeView} from "./views/HomeView";
+import {OrderView} from "./views/OrderView";
+import {Footer} from "./components/Footer/Footer";
 
 
 function App() {
-//     const [seedsData, setSeedsData] = useState([]); // Początkowo dane są puste tablicy
-//
-//     useEffect(() => {
-//         // Wykonaj żądanie do serwera, aby pobrać dane nasionek
-//         fetch('http://localhost:3001/') // Dostosuj URL do swojego backendu
-//             .then((response) => response.json())
-//             .then((data) => {
-//                 setSeedsData(data); // Ustaw dane nasionek w stanie komponentu po pobraniu
-//             })
-//             .catch((error) => {
-//                 console.error('Błąd pobierania danych nasionek:', error);
-//             });
-//     }, []); // Pusty [] oznacza, że ten efekt będzie wywołany tylko raz po montażu komponentu
-
-    // const handleSeedsChange = (newSeeds: SeedEntity) => {
-    //     // Tutaj możesz dodać logikę do obsługi zmian w danych nasionek
-    //     console.log("Nowe dane nasionek:", newSeeds);
-    // };
-
     return (
         <>
-            <Header />
-<Routes>
-<Route path="/" element={<SeedsListTest/>}/>
-</Routes>
+            <Routes>
+                <Route path="/" element={<HomeView/>}/>
+                <Route path="/seed" element={<SeedsList/>}/>
+                <Route path='/order' element={<OrderView/>}/>
+            </Routes>
+            <Footer/>
         </>
     );
 }
