9890d03aeb0c919e6880b5e7af7653c959904580
Katarzyna Barska <104527962+Kasia7891@users.noreply.github.com>
2023-10-10 15:31:51 +0200

    Added SummaryView component.

---
+++ src/views/SummaryView.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {Logo} from "../components/Logo/Logo";
+import {NavBar} from "../components/NavBar/NavBar";
+import {Footer} from "../components/Footer/Footer";
+
+
+
+
+
+export const SummaryView = () => {
+    return(
+    <>
+    <Logo/>
+        <NavBar/>
+        <Footer/>
+    </>
+    )
+}
