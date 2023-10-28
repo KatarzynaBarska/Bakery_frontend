89426189b5836f1eabd539e100aa58a1107164a3
Katarzyna Barska <104527962+Kasia7891@users.noreply.github.com>
2023-10-28 08:19:02 +0200

    Add SingInView.tsx

---
+++ src/views/SingInView.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SignInSide from '../components/Forms/SignInForm';
+import {Logo} from "../components/Logo/Logo";
+import {NavBar} from "../components/NavBar/NavBar";
+import {Footer} from "../components/Footer/Footer";
+
+export const SingInView = () => {
+    return (
+        <>
+            <Logo/>
+            <NavBar/>
+            <SignInSide/>
+            <Footer/>
+        </>
+    )
+}
