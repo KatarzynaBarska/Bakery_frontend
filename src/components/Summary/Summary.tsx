09680fd2e3925fc45561bb44ef43e12c7a632e54
Katarzyna Barska <104527962+Kasia7891@users.noreply.github.com>
2023-10-31 16:06:01 +0100

    Add Summary

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {NavLink} from "react-router-dom";
+import {SeedEntity} from "types";
 
 import './Summary.css';
 
-// interface Props {
-// bases: BaseEntity[];
-// seedsList: SeedEntity[];
-// };
-export const Summary = () => {
-return (
-    <div className="Summary_container">
-        <p>
-            Posumowanie zamówienia
-        </p>
-<hr/>
-        <button><NavLink to="/order">Wróć do strony główniej</NavLink></button>
-    </div>
-)
+interface Props {
+    seeds: SeedEntity[];
+}
+
+export const Summary = (props: Props) => {
+    const [totalSeedsCount, setTotalSeedsCount] = useState<number | null>(null);
+    const [totalPrice, setTotalPrice] = useState<number | null>(null);
+
+    useEffect(() => {
+        const newTotalSeedsCount = props.seeds.reduce((total, seed) => total + seed.count, 0);
+        const newTotalPrice = props.seeds.reduce((total, seed) => total + seed.count * seed.price, 0);
+        setTotalSeedsCount(newTotalSeedsCount);
+        setTotalPrice(newTotalPrice);
+    }, [props.seeds]);
+
+
+    return (
+        <>
+            <div className="Summary__container">
+                <p>
+                    Podsumowanie zamówienia: do zapłaty <strong>{totalPrice}</strong> zł.
+                </p>
+
+                <hr/>
+
+                <button><NavLink to="/">Wróć do strony główniej</NavLink></button>
+            </div>
+
+        </>
+    )
 }
