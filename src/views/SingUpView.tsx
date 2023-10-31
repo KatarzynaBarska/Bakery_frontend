027e1f1225815b58f23dcd8e79aa24deb4500e13
Katarzyna Barska <104527962+Kasia7891@users.noreply.github.com>
2023-10-31 16:04:17 +0100

    Add Sign Up form

--- src/views/SingUpView.tsx
+++ src/views/SingUpView.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import {Logo} from "../components/Logo/Logo";
 import {NavBar} from "../components/NavBar/NavBar";
 import {Footer} from "../components/Footer/Footer";
-import {SignUpForm} from "../components/Forms/SignUpForm";
-
+import SignUp from "../components/Forms/SignUpFormMaterialUi";
 
 export const SingUpView = () => {
     return (
         <>
             <Logo/>
             <NavBar/>
-            <h1>Formularz rejestracyjny</h1>
-            <SignUpForm/>
+            <SignUp/>
             <Footer/>
         </>
     );
