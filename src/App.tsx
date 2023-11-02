fec184bd265fbd3a0aed138b3abca22f7d138e58
Katarzyna Barska <104527962+Kasia7891@users.noreply.github.com>
2023-11-02 17:24:22 +0100

    Change path '/galery' to '/gallery'

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,20 @@ import {SingUpView} from "./views/SingUpView";
 import {SingInView} from "./views/SingInView";
 import {CalendarView} from "./views/CalendarView";
 import {GalleryView} from "./views/GalleryView";
-import { HomePageView } from './views/HomePageView';
+import {HomePageView} from './views/HomePageView';
+import {AboutView} from "./views/AboutView";
 
 function App() {
     return (
         <>
             <Routes>
-                {/*<Route path="/" element={<HomeView/>}/>*/}
                 <Route path="/" element={<HomePageView/>}/>
                 <Route path='/order' element={<OrderView/>}/>
                 <Route path='/rej' element={<SingUpView/>}/>
                 <Route path='/log' element={<SingInView/>}/>
                 <Route path='/calender' element={<CalendarView/>} />
-                <Route path='/galery' element={<GalleryView/>}/>
+                <Route path='/gallery' element={<GalleryView/>}/>
+                {/*<Route path='/about' element={<AboutView/>} />*/}
             </Routes>
             </>
     );
